b28406251f7dde313417e3ce534e19696a7524a7
Chris Farmer <farmerc@udel.edu>
2020-02-20 17:12:19 -0500

    Finish

--- src/main/java/io/zipcoder/polymorphism/Fish.java
+++ src/main/java/io/zipcoder/polymorphism/Fish.java
@@ -1,4 +1,11 @@
 package io.zipcoder.polymorphism;
 
-public class Fish {
+public class Fish extends Pet{
+    public Fish(String name) {
+        super(name);
+    }
+
+    public String speak() {
+        return "Blub";
+    }
 }
